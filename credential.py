11209eb77c12f4065733cd0a68b5056bb0af0dac
Silvia <silvianjoki@Silvias-MacBook-Air.local>
2022-02-20 23:18:16 +0300

    add test

--- credential.py
+++ credential.py
@@ -7,9 +7,9 @@ class Credential:
     
     credential_list = []
     
-    def __init__(self, application , account_username, account_password) -> None:
+    def __init__(self, application_information , account_username, account_password) -> None:
         
-        self.application = application
+        self.application = application_information
         self.account_username = account_username
         self.account_password = account_password
         
