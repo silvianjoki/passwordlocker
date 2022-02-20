83c8206d60f8995880fa09c5626b91cc8299b006
Silvia <silvianjoki@Silvias-MacBook-Air.local>
2022-02-20 23:21:32 +0300

    add append method

---
+++ user-test.py
@@ -0,0 +1,11 @@
+from signal import default_int_handler
+
+
+class User:
+    
+    # generates new user instance 
+    
+    user_list = []
+    
+    def __init__(self, user_name, password) -> None:
+        pass
