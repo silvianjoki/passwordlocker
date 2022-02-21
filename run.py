7c1278ef163d419c876edb5605e432e7dcf0ec84
Silvia <silvianjoki@Silvias-MacBook-Air.local>
2022-02-21 08:01:16 +0300

    add password requirements

--- run.py
+++ run.py
@@ -1,15 +1,35 @@
 import random
 import string
 
-class Credential: 
+def main ():
     
-    # creates new credentials for users
-    
-    credential_list = []
-    
-    def __init__(self, application_information , account_username, account_password) -> None:
+    while True: 
+        print ('Hello, welcome to passwordlocker!')
+        print('\n')
         
-        self.application = application_information
-        self.account_username = account_username
-        self.account_password = account_password
-        
+        if short_code == 'nu':
+            print('create username')
+            created_user_name = input ()
+            
+            print ('create password')
+            created_user_password = input()
+            
+            print ('confirm your password')
+            confirm_password = input()
+            
+            while confirm_password != created_user_password:
+                print('your passwords did not match try again!')
+                print ('enter your password again')
+                created_user_password= input()
+                print ('confirm your password')
+                confirm_password = input()
+            else:
+                print (f'congrats {created_user_name} account creation was a success! ')
+                print('\n')
+                print('proceed to login now')
+                print(username)
+                entered_username = input()
+                print('your password please')
+                entered_password = input()
+                
+            while entered_username != created_user_name or entered_password != created_user_password:
